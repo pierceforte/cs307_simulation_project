124d041893958132cc84fe031cfdc53f32f7b525
Suomo Ammah <sna19@duke.edu>
2020-03-07 00:16:39 -0500

    switching branch

--- test/cellsociety/RPSTest.java
+++ test/cellsociety/RPSTest.java
@@ -1,5 +1,15 @@
 package cellsociety;
 
+
+import cellsociety.simulation.RPSModel;
+import org.junit.jupiter.api.Test;
+
 public class RPSTest extends CellSocietyTest {
 
+    @Test
+    public void testGOLGridPopulation() {
+        testGridPopulation(RPSModel.class, "RPSConfig");
+    }
 }
+
+
