51f1dc91d28ae7ebe67f959838cf5f167b47d6c9
Suomo Ammah <sna19@duke.edu>
2020-03-07 02:34:55 -0500

    test rps

--- test/cellsociety/RPSTest.java
+++ test/cellsociety/RPSTest.java
@@ -1,8 +1,12 @@
 package cellsociety;
 
 
+import cellsociety.cell.Cell;
+import cellsociety.grid.Grid;
 import cellsociety.simulation.RPSModel;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RPSTest extends CellSocietyTest {
 
@@ -10,6 +14,27 @@ public class RPSTest extends CellSocietyTest {
     public void testGOLGridPopulation() {
         testGridPopulation(RPSModel.class, "RPSConfig");
     }
+
+    @Test
+    public void teststate(){
+        testRPSCellStateChange(4,4,"5","9");
+
+        testRPSCellStateChange(2,1,"8","8");
+    }
+    private void testRPSCellStateChange(int row, int col, String initialState, String updatedStated) {
+        createModelFromStart(RPSModel.class);
+        Grid cells = getMySimModel().getGrid();
+
+        // get a cell
+        Cell loneCell = cells.get(row, col);
+        // assert that this cell's initial state is correct
+        assertEquals(initialState, loneCell.getState());
+        // update simulation (one step)
+        getMySimModel().update();
+        // assert that this cell's updated state is correct
+        assertEquals(updatedStated, loneCell.getState());
+    }
+
 }
 
 
